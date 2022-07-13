3d7b5026339a5d6c071857a532acb4cdb8a52267
MUKUL BANSAL <curlvector747@gmail.com>
2022-07-14 00:04:43 +0530

    Create Painting the Fence.cpp

---
+++ Dynamic programming/Painting the Fence.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    long long countWays(int n, int k){
+        // code here
+        long long mod=1e9+7;
+     
+     if(n==0)
+     return 0;
+     
+     if(n==1)
+     return k%mod;
+     
+     
+     long long same=k%mod;
+     long long diff= (k*(k-1))%mod;
+     
+     for(long long  i=3;i<=n;i++)
+     {
+         long long prev=diff%mod;
+        diff=((same+prev)*(k-1))%mod;
+          same=prev%mod;
+     }
+     
+     return (same+diff)%mod;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;
+		Solution ob;
+		cout<<ob.countWays(n,k)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
