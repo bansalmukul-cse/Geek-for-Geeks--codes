bef130fad7cb3f85d86cc624135e3ddd13f7bf1a
MUKUL BANSAL <curlvector747@gmail.com>
2022-07-17 16:48:44 +0530

    Create Longest Increasing Subsequence.cpp

---
+++ Dynamic programming/Longest Increasing Subsequence.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find length of longest increasing subsequence.
+    int longestSubsequence(int n, int a[])
+    {
+       // your code here
+     int dp[n+1];
+        for(int i=1;i<=n;i++)
+        dp[i]=INT_MAX;
+        
+        dp[0]=INT_MIN;
+        
+        for(int i=0;i<n;i++)
+        {
+            int index=upper_bound(dp,dp+n+1,a[i])-dp;
+            
+            if(a[i]>dp[index-1] && a[i]<dp[index])
+            dp[index]=a[i];
+            
+        }
+        
+        int ans=0;
+        for(int i=n;i>=0;i--)
+        {
+           if(dp[i]!=INT_MAX)
+           {
+               ans=i;
+               break;
+           }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        //taking size of array
+        cin>>n;
+        int a[n];
+        
+        //inserting elements to the array
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        //calling method longestSubsequence()
+        cout << ob.longestSubsequence(n, a) << endl;
+    }
+}
+  // } Driver Code Ends
